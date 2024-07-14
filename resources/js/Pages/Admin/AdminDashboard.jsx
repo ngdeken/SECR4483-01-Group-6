0072322a3703d941bc0d50dde2edcd1f8c66f503
ngdeken <63795244+ngdeken@users.noreply.github.com>
2024-07-15 00:22:10 +0800

    Removed admin status table

--- resources/js/Pages/Admin/AdminDashboard.jsx
+++ resources/js/Pages/Admin/AdminDashboard.jsx
@@ -3,9 +3,7 @@ import Sidebar from '../../Components/AdminSidebar';
 import '../../../css/StaffHome.css'; 
 
 const HomePage = ({auth}) => {
-    const statusCards = [
-        { title: 'User', status: '4', color: '#d4b06a' },
-    ];
+    
 
     return (
         <div className="app-container">
@@ -14,14 +12,7 @@ const HomePage = ({auth}) => {
                 <header className="header">
                     <h1>Welcome back, {auth.user.name}!</h1>
                 </header>
-                <div className="status-cards">
-                    {statusCards.map((card, index) => (
-                        <div key={index} className="status-card" style={{ borderColor: card.color }}>
-                            <h2>{card.title}</h2>
-                            <p>{card.status}</p>
-                        </div>
-                    ))}
-                </div>
+                
             </div>
         </div>
     );
